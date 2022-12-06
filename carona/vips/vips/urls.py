c1ed2cf3919c7bf19f0229ff33379563e9537429
vipulgithub18 <vipulgithub18@gmail.com>
2022-12-06 16:18:33 +0530

    my second commit

--- carona/vips/vips/urls.py
+++ carona/vips/vips/urls.py
@@ -1,4 +1,4 @@
-"""carona URL Configuration
+"""vips URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -18,5 +18,5 @@ from django.urls import path
 from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("home/",views.student),
+    path('home/',views.student),
 ]
